fbcef58dab5c20514be4ef54411fe024d6707ea4
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-09-19 11:09:55 -0400

    Sort registrants by name, phone, email

--- server/publications.js
+++ server/publications.js
@@ -5,5 +5,5 @@ Meteor.publish('singleEvent', function(id) {
   return id && Events.find(id);
 });
 Meteor.publish('registrants', function(id) {
-  return id && Registrants.find({eventIds: {$elemMatch: { $in: [id]}}});
+  return id && Registrants.find({eventIds: {$elemMatch: { $in: [id]}}}, {sort: {name:1, phone:1, email:1}});
 });
